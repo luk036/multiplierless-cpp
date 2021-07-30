41b7e5d65a384f8b1038bc2040f7f4a320e328e0
Wai-Shing Luk <luk036@gmail.com>
2021-07-30 21:05:10 +0800

    Initial commit

---
+++ source/greeter.cpp
@@ -0,0 +1,20 @@
+#include <bingo/greeter.h>
+#include <fmt/format.h>
+
+using namespace bingo;
+
+Bingo::Bingo(std::string _name) : name(std::move(_name)) {}
+
+std::string Bingo::greet(LanguageCode lang) const {
+    switch (lang) {
+        default:
+        case LanguageCode::EN:
+            return fmt::format("Hello, {}!", name);
+        case LanguageCode::DE:
+            return fmt::format("Hallo {}!", name);
+        case LanguageCode::ES:
+            return fmt::format("¡Hola {}!", name);
+        case LanguageCode::FR:
+            return fmt::format("Bonjour {}!", name);
+    }
+}
---
+++ test/source/test_integers.cpp
@@ -0,0 +1,32 @@
+#include <limits> // import numric_limits
+#include <type_traits> // import make_signed
+
+#include <doctest/doctest.h>
+
+template <typename T> 
+static void check_unsigned() {
+    auto MAX = std::numeric_limits<T>::max();
+
+    auto a = T{MAX};
+    auto v = T{MAX/2};
+    auto z = T{0};
+    auto a2 = std::make_signed_t<T>(a - v);
+    auto z2 = std::make_signed_t<T>(z - v);
+    auto a3 = -a2;
+    auto z3 = -z2;
+    auto a4 = -a3;
+    auto z4 = -z3;
+    auto a5 = T(a4 + v);
+    auto z5 = T(z4 + v);
+
+    CHECK(a == a5);
+    CHECK(z == z5);
+}
+
+TEST_CASE("test unsigned long long") {
+    check_unsigned<unsigned long long>();
+}
+
+TEST_CASE("test unsigned long") {
+    check_unsigned<unsigned long>();
+}
