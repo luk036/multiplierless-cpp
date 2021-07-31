74530ae0d9b56c17a118cbaa3cb91cc86b10e980
Wai-Shing Luk <luk036@gmail.com>
2021-07-31 12:08:06 +0000

    fix the build

--- specific.cmake
+++ specific.cmake
@@ -7,36 +7,36 @@ CPMAddPackage(
   OPTIONS "FMT_INSTALL YES" # create an installable target
 )
 
-find_package(OpenBLAS REQUIRED)
-if(OpenBLAS_FOUND)
-  message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
-  # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE ${OpenBLAS_INCLUDE_DIRS})
-  include_directories(${OpenBLAS_INCLUDE_DIRS})
-endif(OpenBLAS_FOUND)
+# find_package(OpenBLAS REQUIRED)
+# if(OpenBLAS_FOUND)
+#   message(STATUS "Found OpenBLAS: ${OpenBLAS_LIBRARIES}")
+#   # target_include_directories(OpenBLAS::OpenBLAS SYSTEM INTERFACE ${OpenBLAS_INCLUDE_DIRS})
+#   include_directories(${OpenBLAS_INCLUDE_DIRS})
+# endif(OpenBLAS_FOUND)
 
-find_package(LAPACK REQUIRED)
-if(LAPACK_FOUND)
-  message(STATUS "Found LAPACK: ${LAPACK_LIBRARIES}")
-  include_directories(${LAPACK_INCLUDE_DIRS})
-endif(LAPACK_FOUND)
+# find_package(LAPACK REQUIRED)
+# if(LAPACK_FOUND)
+#   message(STATUS "Found LAPACK: ${LAPACK_LIBRARIES}")
+#   include_directories(${LAPACK_INCLUDE_DIRS})
+# endif(LAPACK_FOUND)
 
-find_package(BLAS REQUIRED)
-if(BLAS_FOUND)
-  message(STATUS "Found BLAS: ${BLAS_LIBRARIES}")
-  include_directories(${BLAS_INCLUDE_DIRS})
-endif(BLAS_FOUND)
+# find_package(BLAS REQUIRED)
+# if(BLAS_FOUND)
+#   message(STATUS "Found BLAS: ${BLAS_LIBRARIES}")
+#   include_directories(${BLAS_INCLUDE_DIRS})
+# endif(BLAS_FOUND)
 
-# CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
-# if(xtl_ADDED)
-#   message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
-#   include_directories(${xtl_SOURCE_DIR}/include)
-# endif(xtl_ADDED)
+CPMAddPackage("gh:xtensor-stack/xtl#0.6.23")
+if(xtl_ADDED)
+  message(STATUS "Found xtl: ${xtl_SOURCE_DIR}")
+  include_directories(${xtl_SOURCE_DIR}/include)
+endif(xtl_ADDED)
 
-# CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
-# if(xtensor_ADDED)
-#   message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
-#   include_directories(${xtensor_SOURCE_DIR}/include)
-# endif(xtensor_ADDED)
+CPMAddPackage("gh:xtensor-stack/xtensor#0.22.0")
+if(xtensor_ADDED)
+  message(STATUS "Found xtensor: ${xtensor_SOURCE_DIR}")
+  include_directories(${xtensor_SOURCE_DIR}/include)
+endif(xtensor_ADDED)
 
 CPMAddPackage(
   NAME EllAlgo
@@ -45,18 +45,19 @@ CPMAddPackage(
   OPTIONS "INSTALL_ONLY YES" # create an installable target
 )
 
+# CPMAddPackage("gh:xtensor-stack/xtensor-blas#0.18.0")
+# if(xtensor-blas_ADDED)
+#   message(STATUS "Found xtensor-blas: ${xtensor-blas_SOURCE_DIR}")
+#   include_directories(${xtensor-blas_SOURCE_DIR}/include)
+# endif(xtensor-blas_ADDED)
+# # remember to turn off the warnings
 
-CPMAddPackage("gh:xtensor-stack/xtensor-blas#0.18.0")
-if(xtensor-blas_ADDED)
-  message(STATUS "Found xtensor-blas: ${xtensor-blas_SOURCE_DIR}")
-  include_directories(${xtensor-blas_SOURCE_DIR}/include)
-endif(xtensor-blas_ADDED)
-# remember to turn off the warnings
+# if(WIN32)
+#   add_definitions(-DXTENSOR_USE_FLENS_BLAS)
+# endif()
 
-if(WIN32)
-  add_definitions(-DXTENSOR_USE_FLENS_BLAS)
-endif()
+# set(SPECIFIC_LIBS EllAlgo::EllAlgo ${OpenBLAS_LIBRARIES} ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES}
+#                   fmt::fmt
+# )
 
-set(SPECIFIC_LIBS EllAlgo::EllAlgo ${OpenBLAS_LIBRARIES} ${LAPACK_LIBRARIES} ${BLAS_LIBRARIES}
-                  fmt::fmt
-)
+set(SPECIFIC_LIBS EllAlgo::EllAlgo fmt::fmt)
