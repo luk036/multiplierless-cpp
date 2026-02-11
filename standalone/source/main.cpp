e1f31bd11aa2e91f676b962fa9edb4e2c4fef87d
Wai-Shing Luk <luk036@gmail.com>
2026-02-11 22:34:38 +0800

    add back standalone build

--- standalone/source/main.cpp
+++ standalone/source/main.cpp
@@ -5,17 +5,17 @@
 #include <string>         // for char_traits, hash, operator==
 #include <unordered_map>  // for operator==, unordered_map, __has...
 
-auto main(int argc, char **argv) -> int {
-    const std::unordered_map<std::string, multiplierless::LanguageCode> languages{
-        {"en", multiplierless::LanguageCode::EN},
-        {"de", multiplierless::LanguageCode::DE},
-        {"es", multiplierless::LanguageCode::ES},
-        {"fr", multiplierless::LanguageCode::FR},
-    };
+auto main(int argc, char** argv) -> int {
+    // const std::unordered_map<std::string, multiplierless::LanguageCode> languages{
+    //     {"en", multiplierless::LanguageCode::EN},
+    //     {"de", multiplierless::LanguageCode::DE},
+    //     {"es", multiplierless::LanguageCode::ES},
+    //     {"fr", multiplierless::LanguageCode::FR},
+    // };
 
     cxxopts::Options options(*argv, "A program to welcome the world!");
 
-    std::string language;
+    // std::string language;
     std::string name;
 
     // clang-format off
@@ -23,7 +23,7 @@ auto main(int argc, char **argv) -> int {
     ("h,help", "Show help")
     ("v,version", "Print the current version number")
     ("n,name", "Name to greet", cxxopts::value(name)->default_value("World"))
-    ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
+    // ("l,lang", "Language code to use", cxxopts::value(language)->default_value("en"))
   ;
     // clang-format on
 
@@ -39,11 +39,11 @@ auto main(int argc, char **argv) -> int {
         return 0;
     }
 
-    auto langIt = languages.find(language);
-    if (langIt == languages.end()) {
-        std::cerr << "unknown language code: " << language << std::endl;
-        return 1;
-    }
+    // auto langIt = languages.find(language);
+    // if (langIt == languages.end()) {
+    //     std::cerr << "unknown language code: " << language << std::endl;
+    //     return 1;
+    // }
 
     // multiplierless::Multiplierless multiplierless(name);
     // std::cout << multiplierless.greet(langIt->second) << std::endl;
